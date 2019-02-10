662f741bfc35b843188195d980c3a9f82fcef727
WookieHere <47507330+WookieHere@users.noreply.github.com>
2019-02-10 18:32:08 -0500

    Add files via upload

--- Zelda_Engine.js
+++ Zelda_Engine.js
@@ -1,37 +1,28 @@
 //board is 9x9 so 81 spaces
-const board_Size  = 81;
-const rows = Math.sqrt(board_Size);
-const cols = Math.sqrt(board_Size);
-const initialX = 5;
-const initialY = 9;
-const player_ID = "player";
-var walkingSpaces = [board_Size];
+const board_Size 81;
+const rows sqrt(board_Size);
+const cols sqrt(board_Size);
+const initialX 5;
+const initialY 9;
+var walkingSpaces[board_Size];
 var x, y;
 
-function generateStart()
+class player(initialX, initialY)
 {
-  var i;
-  for (i = 0; i < board_Size; i++)
+  function init()
   {
-    if(initialX == i%rows && initialY == Math.floor(board_Size/cols))
-    {
-      walkingSpaces[i] = player_ID;
-    }else{
-      walkingSpaces[i] = "None";
-    }
-  }
-}
-
-class player
-{
-  constructor(initialX, initialY)
-  {
-    this.x = initialX;
-    this.y = initialY;
+    self.x = initialX;
+    self.y = initialY;
+    /*println(getPlayerIndex());
+    document.write(getPlayerIndex());
+    document.write(getPlayerX());
+    document.write(getPlayerY());*/
+    println("output of println");
+    document.write("output of doc.write");
   }
 
 
-  get PlayerIndex()
+  function getPlayerIndex()
   {
     var i;
     for (i = 0; i < board_Size; i++)
@@ -43,27 +34,26 @@ class player
     }
   }
 
-  get PlayerX()
+  function getPlayerX()
   {
-    var x = PlayerIndex() % 9;
+    var x = getPlayerIndex() % 9;
     return x;
   }
 
-  get PlayerY()
+  function getPlayerY()
   {
-    var y = Math.floor(PlayerIndex()/9);
+    var y = floor(getPlayerIndex()/9);
     return y;
   }
 
 }
 
-
-function printTest()
+function print_test()
 {
-  document.write(protagonist.PlayerIndex());
+  println("output of println");
+  document.write("output of document.write");
+  document.getElementById("printTest").innerHTML = "output of getElementById";
+  //git@githup.com:CW1118/fantasy-game.git
 }
 
-generateStart();
-let protagonist = new player()
-
-//document.getElementById(printTest).innerHTML = protagonist.PlayerX();
+protagonist = player(initialX, initialY);
