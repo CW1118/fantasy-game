87506533ca1b867d4e51f0c426de7746a600fe7c
Luke Nasby <wookiehere99@gmail.com>
2019-02-10 18:00:31 -0500

    Update Zelda_Engine.js

--- Zelda_Engine.js
+++ Zelda_Engine.js
@@ -4,26 +4,21 @@ const rows = Math.sqrt(board_Size);
 const cols = Math.sqrt(board_Size);
 const initialX = 5;
 const initialY = 9;
+const player_ID = "player";
 var walkingSpaces = [];
 var x, y;
 
-/*
-class player(initialX, initialY)
+
+class player
 {
-  function init()
+  constructor(initialX, initialY)
   {
-    self.x = initialX;
-    self.y = initialY;
-    /*println(getPlayerIndex());
-    document.write(getPlayerIndex());
-    document.write(getPlayerX());
-    document.write(getPlayerY());
-    println("output of println");
-    document.write("output of doc.write");
+    this.x = initialX;
+    this.y = initialY;
   }
 
 
-  function getPlayerIndex()
+  get PlayerIndex()
   {
     var i;
     for (i = 0; i < board_Size; i++)
@@ -35,20 +30,20 @@ class player(initialX, initialY)
     }
   }
 
-  function getPlayerX()
+  get PlayerX()
   {
     var x = getPlayerIndex() % 9;
     return x;
   }
 
-  function getPlayerY()
+  get PlayerY()
   {
-    var y = floor(getPlayerIndex()/9);
+    var y = Math.floor(getPlayerIndex()/9);
     return y;
   }
 
 }
-*/
+
 function printTest()
 {
   //println("output of println");
