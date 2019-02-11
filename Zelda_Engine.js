14c8f29b695b1a905d0f6df7257cf2d007c66fff
WookieHere <47507330+WookieHere@users.noreply.github.com>
2019-02-10 19:07:27 -0500

    Add files via upload

--- Zelda_Engine.js
+++ Zelda_Engine.js
@@ -1,9 +1,9 @@
 //board is 9x9 so 81 spaces
 const board_Size  = 81;
-const rows = Math.sqrt(board_Size);
-const cols = Math.sqrt(board_Size);
-const initialX = 5;
-const initialY = 9;
+const rows = 9;
+const cols = 9;
+var initialX = 5;
+var initialY = 9;
 const player_ID = "player";
 var walkingSpaces = [board_Size];
 var x, y;
@@ -13,7 +13,7 @@ function generateStart()
   var i;
   for (i = 0; i < board_Size; i++)
   {
-    if(initialX == i%rows && initialY == Math.floor(board_Size/cols))
+    if((initialX == (i % rows)) && (initialY == Math.ceil(i/cols)))
     {
       walkingSpaces[i] = player_ID;
     }else{
@@ -24,10 +24,11 @@ function generateStart()
 
 class player
 {
-  constructor(initialX, initialY)
+  constructor(initialX, initialY, walkingSpaces)
   {
     this.x = initialX;
     this.y = initialY;
+    this.walkingSpaces = walkingSpaces;
   }
 
 
@@ -45,13 +46,13 @@ class player
 
   get PlayerX()
   {
-    var x = PlayerIndex() % 9;
+    var x = (PlayerIndex() % 9);
     return x;
   }
 
   get PlayerY()
   {
-    var y = Math.floor(PlayerIndex()/9);
+    var y = (Math.floor(PlayerIndex()/9));
     return y;
   }
 
@@ -60,7 +61,9 @@ class player
 
 function printTest()
 {
-  document.write(protagonist.PlayerIndex());
+  document.write(protagonist.PlayerIndex);
+  document.write(protagonist.PlayerX);
+  document.write(protagonist.PlayerY);
 
 }
 
